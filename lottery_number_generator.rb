1d85fd9686711309ce035a42253dd9ea43684682
Laura Coursen <lecoursen@github.com>
2020-03-10 08:52:44 -0500

    Fix indents

--- lottery_number_generator.rb
+++ lottery_number_generator.rb
@@ -1,10 +1,10 @@
 def lottery_number_generator(i = 6)
   arr = []
-    while arr.length < i
-      num = rand(50)
-      arr << num unless arr.include?(num)
-    end
-    arr
+  while arr.length < i
+    num = rand(50)
+    arr << num unless arr.include?(num)
+  end
+  arr
 end
 
 6.times { p lottery_number_generator }
